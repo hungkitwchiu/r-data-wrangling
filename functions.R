635225361d282ee4233eb186d111ac59b5d8ef39
hkwilliamchiu <77698891+hkwilliamchiu@users.noreply.github.com>
2024-11-06 17:15:23 -0800

    Update functions.R

--- functions.R
+++ functions.R
@@ -8,7 +8,7 @@ pacman::p_load("readxl","data.table","purrr","stringr","dplyr")
 # ------------------------------------------------------------------------------
 # Coalesce join, duplicate column names
 # ------------------------------------------------------------------------------
-coalesce_join <- function(data.list, by = NULL, suffix = c(".x", ".y"), join = dplyr::full_join){
+coalesce.join <- function(data.list, by = NULL, suffix = c(".x", ".y"), join = dplyr::full_join){
   names <- unlist(lapply(temp, function(x){colnames(x)}))
   duplicates <- names[which(duplicated(names))][names[which(duplicated(names))] != by]
   merged.data <- data.list %>%
@@ -30,6 +30,28 @@ mapview.with.shape.data <- function(data.interest, data.shape, var.interest, lin
     right_join(data.interest[c(linkage, var.interest, var.time)], by = linkage) %>%
     filter(!!rlang::sym(var.time) == time)
   mapview(temp, zcol = var.interest)
+}
+
+show.did.plot = function(gdat, x.name, y.name, t.name, vlines, show.means, pos.means = NULL){
+  gdat <- gdat %>% mutate(group = if_else(!!sym(t.name) == 0, "Control", "Treatment")) # supports only 1 treatment and 1 control
+  gg = ggplot(gdat, aes(y = get(y.name), x = get(x.name), color = group)) +
+    geom_line() + 
+    theme_bw() +
+    labs(x = tools::toTitleCase(x.name), y = tools::toTitleCase(y.name), color = "Group")
+  
+  for (vline in vlines){gg = gg + geom_vline(xintercept = vline)}
+  
+  if(show.means) {
+    exps <- sort(unique(gdat$exp))
+    treats <- sort(unique(gdat$treat))
+    for (exp in exps){
+      for (treat in treats){
+        y.label <- mean(unlist(filter(gdat, treat==!!treat, exp==!!exp)[, y.name])) %>% round(1)
+        gg = gg + annotate("label", x=pos.means[exp+1], y=y.label*1.01, label=y.label)
+      }
+    }
+  }
+  gg
 }
 
   # coalesce specified items
