357be6324dba55faac9b6a2dbc6689fb4ebeaf4e
hkwilliamchiu <77698891+hkwilliamchiu@users.noreply.github.com>
2024-11-09 20:46:17 -0800

    Update functions.R

--- functions.R
+++ functions.R
@@ -51,6 +51,15 @@ coalesce.join <- function(data.list, id, arrange.col = NULL, co.names = NULL, jo
   return(merged.data)
 }
 
+# ============================================================================================================
+mapview.with.shape.data <- function(data.interest, data.shape, var.interest, linkage, var.time, time){
+  temp <- data.shape %>%
+    right_join(data.interest[c(linkage, var.interest, var.time)], by = linkage) %>%
+    filter(!!rlang::sym(var.time) == time)
+  mapview(temp, zcol = var.interest)
+}
+
+# ============================================================================================================
 show.did.plot = function(gdat, x.name, y.name, t.name, vlines, show.means, pos.means = NULL){
   gdat <- gdat %>% mutate(group = if_else(!!sym(t.name) == 0, "Control", "Treatment")) # supports only 1 treatment and 1 control
   gg = ggplot(gdat, aes(y = get(y.name), x = get(x.name), color = group)) +
