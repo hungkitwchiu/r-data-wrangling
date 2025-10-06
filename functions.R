554a32d487dc18381a65fdaacfaa1c802a9e14e0
Hung Kit (William) Chiu <77698891+hungkitwchiu@users.noreply.github.com>
2025-10-06 16:20:16 -0700

    Update functions.R

--- functions.R
+++ functions.R
@@ -119,12 +119,14 @@ coalesce.join <- function(data.list, id, arrange.col = NULL, co.names = NULL,
 # ------------------------------------------------------------------------------
 mapview.with.shape.data <- function(data.interest, data.shape, var.interest, 
                                     link.interest, link.shape, layer.name = NULL,
+                                    at = NULL, col.regions = NULL, zlim = NULL, 
                                     return = FALSE){
   if (is.null(layer.name)){layer.name = var.interest}
   temp <- data.shape %>%
     right_join(data.interest %>% dplyr::select(eval(link.interest), eval(var.interest)), 
                by = join_by(!!link.shape == !!rlang::sym(link.interest)))
-  m <- mapview(temp, zcol = var.interest, layer.name = layer.name)
+  m <- mapview(temp, zcol = var.interest, layer.name = layer.name, at = at, 
+               col.regions = col.regions, zlim = zlim)
   if (return){return(m)} else {m}
 }
 
